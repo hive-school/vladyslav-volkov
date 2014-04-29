23f36bfa93433a3864ce6259940732de93d33a2d
Vlad Volkov <volkov.vladyslav@gmail.com>
2014-04-29 08:45:13 -0700

    Autoload ex

---
+++ oop/home/src/Building/Window.php
@@ -0,0 +1,6 @@
+<?php
+namespace Building;
+class Window
+{
+
+} 
--- oop/home/test.php
+++ oop/home/test.php
@@ -1,5 +1,13 @@
 <?php
+set_include_path(get_include_path() . PATH_SEPARATOR . __DIR__ . '/src');
+
+spl_autoload_register(
+    function ($className) {
+        require_once(str_replace("\\", "/", $className) . '.php');
+    }
+);
 $school = new School();
+$windows = new Building\Window();
 $student = new Student('Vlad');
 $school->enterToSchool($student);
 foreach ($school->getStudentsInDaHouse() as $studentInDaHouse) {
